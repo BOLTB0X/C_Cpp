5b548908b8022a8b7da6dead65bd7b1a4b2b1733
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-05-30 22:56:29 +0900

    Delete Data_Type.c

--- Data_Type.c
+++
@@ -1,7 +0,0 @@
-int //일반적인 정수형 표현 "%d" 4byte
-long long //숫자가 긴 정수형 표현 "%lld" 8byte
-double,float //일반적인 실수형 표현 "%f" 8byte
-string //문자열 표현 "%s" 헤더 포함해야함 제한X
-bool //참거짓 판별
-char //한 문자형 표현할때 "%c" 1byte
- 

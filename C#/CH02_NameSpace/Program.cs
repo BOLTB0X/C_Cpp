1080b586e07565be3f0124d7072984295bd71731
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-06-26 17:24:18 +0900

    C#

---
+++ C#/CH02_NameSpace/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Ex1
+{
+    class Ex
+    {
+        public static string data = "Ex1";
+    }
+}
+namespace Ex2
+{
+    class Ex
+    {
+        public static string data = "Ex2";
+    }
+}
+namespace CH02_NameSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Ex1.Ex.data);
+            Console.WriteLine(Ex2.Ex.data);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/CH03_Enum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH03_Enum
+{
+    class Program
+    {
+        enum Color { BLACK, RED, BLUE, GREEN, WHITE};
+
+        static void Main(string[] args)
+        {
+            Color color = Color.BLUE;
+            if (color == Color.BLUE)
+            {
+                Console.WriteLine("파란색입니다. (색상: " + color + ")");
+            }
+            Console.ReadLine();
+        }
+    }
+}

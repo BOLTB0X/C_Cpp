5a56e57d31b03a35bcf02cb30ea7b8b152c4258f
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-06-30 20:42:53 +0900

    C#

---
+++ C#/Windows Forms Event Handing/Windows Forms Event Handing/ChildForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Windows_Forms_Event_Handing
+{
+    partial class ChildForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataTextBox = new System.Windows.Forms.TextBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.transferButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dataTextBox
+            // 
+            this.dataTextBox.Location = new System.Drawing.Point(103, 41);
+            this.dataTextBox.Name = "dataTextBox";
+            this.dataTextBox.Size = new System.Drawing.Size(508, 28);
+            this.dataTextBox.TabIndex = 0;
+            this.dataTextBox.TextChanged += new System.EventHandler(this.dataTextBox_TextChanged);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Location = new System.Drawing.Point(247, 90);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(210, 46);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "상태";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // transferButton
+            // 
+            this.transferButton.Location = new System.Drawing.Point(281, 149);
+            this.transferButton.Name = "transferButton";
+            this.transferButton.Size = new System.Drawing.Size(160, 58);
+            this.transferButton.TabIndex = 2;
+            this.transferButton.Text = "데이터 전송";
+            this.transferButton.UseVisualStyleBackColor = true;
+            this.transferButton.Click += new System.EventHandler(this.transferButton_Click);
+            // 
+            // ChildForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.transferButton);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.dataTextBox);
+            this.Name = "ChildForm";
+            this.Text = "ChildForm";
+            this.Load += new System.EventHandler(this.ChildForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox dataTextBox;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button transferButton;
+    }
+}
---
+++ C#/Windows Forms Event Handing/Windows Forms Event Handing/ParentForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Windows_Forms_Event_Handing
+{
+    partial class ParentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openChildForm = new System.Windows.Forms.Button();
+            this.dataLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openChildForm
+            // 
+            this.openChildForm.Location = new System.Drawing.Point(266, 60);
+            this.openChildForm.Name = "openChildForm";
+            this.openChildForm.Size = new System.Drawing.Size(222, 69);
+            this.openChildForm.TabIndex = 0;
+            this.openChildForm.Text = "자식 폼 열기";
+            this.openChildForm.UseVisualStyleBackColor = true;
+            this.openChildForm.Click += new System.EventHandler(this.openChildForm_Click);
+            this.openChildForm.MouseLeave += new System.EventHandler(this.openChildForm_MouseLeave);
+            this.openChildForm.MouseHover += new System.EventHandler(this.openChildForm_MouseHover);
+            // 
+            // dataLabel
+            // 
+            this.dataLabel.AutoSize = true;
+            this.dataLabel.Location = new System.Drawing.Point(351, 164);
+            this.dataLabel.Name = "dataLabel";
+            this.dataLabel.Size = new System.Drawing.Size(62, 18);
+            this.dataLabel.TabIndex = 1;
+            this.dataLabel.Text = "데이터";
+            // 
+            // ParentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataLabel);
+            this.Controls.Add(this.openChildForm);
+            this.Name = "ParentForm";
+            this.Text = "ParentForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openChildForm;
+        private System.Windows.Forms.Label dataLabel;
+    }
+}
